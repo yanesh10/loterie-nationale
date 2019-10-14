7bdd9eedc43a182159da2ac7829bd8b6511732c9
yanesh.caussy <yanesh_07@hotmail.com>
2019-10-14 21:25:59 +0400

    Second commit

---
+++ loterie-nationale-extractor/src/test/java/com/yanesh/loterie/nationale/LoterieNationaleAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yanesh.loterie.nationale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoterieNationaleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
